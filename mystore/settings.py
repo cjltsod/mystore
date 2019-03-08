704b5266eababe31a505192e63815a99ec1b8e15
cjltsod <github.tsod@tsod.idv.tw>
2019-03-09 03:51:24 +0800

    Add shopping cart

--- mystore/settings.py
+++ mystore/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'estore.middleware.cart.CartMiddleware',
 ]
 
 ROOT_URLCONF = 'mystore.urls'
